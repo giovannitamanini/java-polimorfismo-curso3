9284a4af1155168efe5407761750bddcf5369d32
Giovanni Tamanini <giovanni.tnini@gmail.com>
2022-10-23 08:35:53 -0300

    Testando Arrays de referências

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayDeReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayDeReferencias.java
@@ -1,2 +1,24 @@
-package br.com.bytebank.banco.teste;public class TesteArrayDeReferencias {
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteArrayDeReferencias {
+
+    public static void main(String[] args) {
+
+        ContaCorrente[] contas = new ContaCorrente[5]; // Nesta linha é criada uma Array que pode guardar 5 referências do tipo Conta Corrente
+
+        ContaCorrente cc1 = new ContaCorrente(22, 11);
+        contas[0] = cc1;
+
+        ContaCorrente cc2 = new ContaCorrente(22, 22);
+        contas[1] = cc2;
+
+        System.out.println(contas[1].getNumero());
+
+        ContaCorrente ref = contas[1];
+
+        System.out.println(cc2.getNumero());
+        System.out.println(ref.getNumero());
+    }
 }
