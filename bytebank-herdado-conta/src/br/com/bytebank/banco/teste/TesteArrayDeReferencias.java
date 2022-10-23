e05b1cedd0463c36cb9b6dbfaa7f353ab5761c07
Giovanni Tamanini <giovanni.tnini@gmail.com>
2022-10-23 09:05:23 -0300

    Treinando com Array do tipo Object

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayDeReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayDeReferencias.java
@@ -1,30 +1,33 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayDeReferencias {
 
     public static void main(String[] args) {
 
-        ContaCorrente[] contas = new ContaCorrente[5]; // Nesta linha é criada uma Array que pode guardar 5 referências do tipo Conta Corrente
+        Object[] referencias = new Object[5]; // Nesta linha é criada uma Array que pode guardar 5 referências do tipo Conta Corrente
 
         ContaCorrente cc1 = new ContaCorrente(22, 11);
-        contas[0] = cc1;
+        referencias[0] = cc1;
 
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
-        contas[1] = cc2;
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+        referencias[1] = cc2;
 
-        System.out.println(contas[1].getNumero());
-
-        ContaCorrente ref = contas[1];
+        Cliente cliente = new Cliente();
+        referencias[2] = cliente;
 
         System.out.println(cc2.getNumero());
+
+        /*Object referenciaGenerica = contas[1];
+
+        System.out.println(contas[1].getNumero());*/
+
+        ContaPoupanca ref = (ContaPoupanca) referencias[1]; //Type Cast
+        System.out.println(cc2.getNumero());
         System.out.println(ref.getNumero());
-
-        int[] refs = {1, 2 ,3 , 4, 5};
-
-        for(int i = 0; i < refs.length; i++) {
-            System.out.println(refs[i]);
-        }
     }
 }
