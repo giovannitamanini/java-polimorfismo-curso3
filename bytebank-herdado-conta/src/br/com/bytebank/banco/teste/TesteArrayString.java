523e472a5cc7211c3b5466e59a3e912229202671
Giovanni Tamanini <giovanni.tnini@gmail.com>
2022-10-24 10:20:03 -0300

    Brincando com a Array args do método main

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayString.java
@@ -4,5 +4,10 @@ public class TesteArrayString {
 
     public static void main(String[] args) {
 
+        System.out.println("Funcionou!!");
+
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
     }
 }
