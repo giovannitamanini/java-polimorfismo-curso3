c100a258afc212285e220f4b4dbd82d95731ac15
Giovanni Tamanini <giovanni.tnini@gmail.com>
2022-10-25 10:43:17 -0300

    Fazendo mais testes com os guardadores

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteGuardadorReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteGuardadorReferencias.java
@@ -3,12 +3,13 @@ package br.com.bytebank.banco.teste;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.GuardadorDeContas;
+import br.com.bytebank.banco.modelo.GuardadorDeReferencias;
 
 public class TesteGuardadorReferencias {
 
     public static void main(String[] args) {
 
-        GuardadorDeContas guardador = new GuardadorDeContas();
+        GuardadorDeReferencias guardador = new GuardadorDeReferencias();
 
         Conta cc = new ContaCorrente(22, 11);
         guardador.adiciona(cc);
@@ -19,7 +20,7 @@ public class TesteGuardadorReferencias {
         int tamanho = guardador.getQuantidadeDeElementos();
         System.out.println(tamanho);
 
-        Conta ref = guardador.getReferencia(1);
+        Conta ref = (Conta) guardador.getReferencia(1);
         System.out.println(ref.getNumero());
     }
 }
